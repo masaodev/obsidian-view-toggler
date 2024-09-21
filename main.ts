5d826369197d038e8e8d6f4279d4b2b5590af8dd
Masao <masaodev.net@gmail.com>
2024-09-21 11:16:11 +0900

    コマンドを3つ追加

--- main.ts
+++ main.ts
@@ -16,9 +16,10 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		// Toggle Viewコマンド追加
 		this.addCommand({
 			id: 'toggle-view',
-			name: 'Toggle View',
+			name: 'Toggle view',
 			callback: () => {
 				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (view) {
@@ -27,17 +28,78 @@ export default class MyPlugin extends Plugin {
 						// Editing view（ライブプレビュー（Live Preview）
 						state.mode = "source";
 						state.source = false;
-						new Notice('Live Preview');
+						new Notice('Live preview mode');
 					} else if (!state.source) {
 						// Editing view（ソースモード）
+						state.mode = "source";
 						state.source = true;
 						new Notice('Source mode');
 					} else {
 						// Reading view
 						state.mode = "preview";
 						state.source = false;
-						new Notice('Reading view')
+						new Notice('Reading view mode')
 					}
+					view.setState(
+						state,
+						{ history: false },
+					);
+				}
+			}
+		});
+
+		// Set live preview modeコマンド追加
+		this.addCommand({
+			id: 'set-live-preview-mode',
+			name: 'Set live preview mode',
+			callback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					const state = view.getState();
+					// Editing view（ライブプレビュー（Live Preview）
+					state.mode = "source";
+					state.source = false;
+					new Notice('Live preview mode');
+					view.setState(
+						state,
+						{ history: false },
+					);
+				}
+			}
+		});
+
+		// Set Source view modeコマンド追加
+		this.addCommand({
+			id: 'set-source-view-mode',
+			name: 'Set source view mode',
+			callback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					const state = view.getState();
+					// Editing view（ソースモード）
+					state.mode = "source";
+					state.source = true;
+					new Notice('Source mode');
+					view.setState(
+						state,
+						{ history: false },
+					);
+				}
+			}
+		});
+
+		// Set reading view modeコマンド追加
+		this.addCommand({
+			id: 'set-reading-view-mode',
+			name: 'Set reading view mode',
+			callback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					const state = view.getState();
+					// Reading view
+					state.mode = "preview";
+					state.source = false;
+					new Notice('Reading view mode');
 					view.setState(
 						state,
 						{ history: false },
